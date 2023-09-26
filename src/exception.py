954a29b5f147a8c73a3c869fc1f285ac717a23ed
bhavaysharma16199 <bhavaysharma16199@gmail.com>
2023-09-26 22:45:59 +0530

    Logger & Exception

--- src/exception.py
+++ src/exception.py
@@ -13,8 +13,20 @@ def error_message_detail(error, error_detail:sys):
 class CustomException(Exception):
 
     def __init__(self, error_message, error_detail:sys):
-        super().__init__(self, error_message)
-        self.error_message = error_message(error_message, error_detail = error_detail)
+        self.error_message = error_message_detail(error_message, error_detail = error_detail)
+        super().__init__(self.error_message)
 
         def __str__(self):
             return self.error_message
+
+"""
+if __name__ == '__main__':
+    logging.info('Logging has started')
+
+    try:
+        a = 1/0
+
+    except Exception as e:
+        logging.info('Dividing by zero')
+        raise CustomException(e, sys)
+""" 
